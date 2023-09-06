53a588db13e3d7ed9cf0c66137c87855b251e8b8
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-09-06 16:21:50 +0200

    werkend

--- pigrakker/src/main/java/com/jasper/pigrakker/PigrakkerApplication.java
+++ pigrakker/src/main/java/com/jasper/pigrakker/PigrakkerApplication.java
@@ -1,7 +1,12 @@
 package com.jasper.pigrakker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class PigrakkerApplication {
@@ -9,5 +14,6 @@ public class PigrakkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PigrakkerApplication.class, args);
     }
+
 
 }
