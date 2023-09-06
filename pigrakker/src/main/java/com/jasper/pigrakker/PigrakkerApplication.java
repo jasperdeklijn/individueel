b072473903dc047f4f811bc2b5e47e46bdcacd5e
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-09-06 15:24:43 +0200

    initial new project

---
+++ pigrakker/src/main/java/com/jasper/pigrakker/PigrakkerApplication.java
@@ -0,0 +1,13 @@
+package com.jasper.pigrakker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigrakkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigrakkerApplication.class, args);
+    }
+
+}
---
+++ pigrakker/src/test/java/com/jasper/pigrakker/PigrakkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.pigrakker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigrakkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
