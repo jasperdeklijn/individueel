2ea5fe4e16e9e077e2b6ab77734fc130383a32bb
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-09-13 11:15:24 +0200

    add jpa an h2

--- pigrakker/src/main/java/com/jasper/pigrakker/controller/MainController.java
+++ pigrakker/src/main/java/com/jasper/pigrakker/controller/MainController.java
@@ -1,35 +1,20 @@
 package com.jasper.pigrakker.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/showViewPage")
-    public String passParametersWithModel(Model model) {
-        Map<String, String> map = new HashMap<>();
-        map.put("spring", "mvc");
-        model.addAttribute("message", "Baeldung");
-        model.mergeAttributes(map);
-        return "view/viewPage";
-    }
-    @GetMapping("/printViewPage")
-    public String passParametersWithModelMap(ModelMap map) {
-        map.addAttribute("welcomeMessage", "welcome");
-        map.addAttribute("message", "Baeldung");
-        return "view/viewPage";
-    }
-    @GetMapping("/goToViewPage")
-    public ModelAndView passParametersWithModelAndView() {
+    @Value("${spring.application.name}")
+    String appName;
+    @GetMapping("/")
+    public ModelAndView homePage() {
         ModelAndView modelAndView = new ModelAndView("view/viewPage");
-        modelAndView.addObject("message", "Baeldung");
+        modelAndView.addObject("appName", appName);
         return modelAndView;
     }
 
