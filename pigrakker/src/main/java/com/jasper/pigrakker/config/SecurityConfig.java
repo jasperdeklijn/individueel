e10c689c6ae5959cc7c295ac2db944347961cb05
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-11-02 11:47:27 +0100

    better role

--- pigrakker/src/main/java/com/jasper/pigrakker/config/SecurityConfig.java
+++ pigrakker/src/main/java/com/jasper/pigrakker/config/SecurityConfig.java
@@ -3,28 +3,51 @@ package com.jasper.pigrakker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach((authority) -> {
+                GrantedAuthority mappedAuthority;
+
+                if (authority instanceof OidcUserAuthority) {
+                    OidcUserAuthority userAuthority = (OidcUserAuthority) authority;
+                    mappedAuthority = new OidcUserAuthority(
+                            "ROLE_ADMIN", userAuthority.getIdToken(), userAuthority.getUserInfo());
+                } else if (authority instanceof OAuth2UserAuthority) {
+                    OAuth2UserAuthority userAuthority = (OAuth2UserAuthority) authority;
+                    mappedAuthority = new OAuth2UserAuthority(
+                            "ROLE_ADMIN", userAuthority.getAttributes());
+                } else {
+                    mappedAuthority = authority;
+                }
+
+                mappedAuthorities.add(mappedAuthority);
+            });
+
+            return mappedAuthorities;
+        };
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -35,11 +58,15 @@ public class SecurityConfig implements WebMvcConfigurer {
                         .requestMatchers("/order/**").authenticated()
                         .anyRequest().permitAll()
                 )
-                .oauth2Login(withDefaults())
+                .oauth2Client(withDefaults())
+                .oauth2Login((oauth2Login) -> oauth2Login
+                        .userInfoEndpoint((userInfo) -> userInfo
+                                .userAuthoritiesMapper(grantedAuthoritiesMapper())
+                        )
+                )
                 .formLogin(form ->
                         form
                                 .loginPage("/login")
-                                .loginProcessingUrl("/login")
                                 .defaultSuccessUrl("/")
                                 .permitAll()
                 )
@@ -50,7 +77,6 @@ public class SecurityConfig implements WebMvcConfigurer {
                 )
                 .build();
     }
-
 
 
 }
