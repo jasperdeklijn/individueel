e3f365feced09f0af000ea1575eed13e6b734b6e
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-12-13 13:09:15 +0100

    remove user

--- src/main/java/com/jasper/pigrakker/repository/ProductRepository.java
+++ src/main/java/com/jasper/pigrakker/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package com.jasper.pigrakker.repository;
 
 import com.jasper.pigrakker.model.Product;
-import com.jasper.pigrakker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 import java.util.Optional;
